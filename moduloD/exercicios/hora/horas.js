e07781ed7182775004617b8e87f852dabd2cd2f8
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-03 18:54:36 -0300

    123

---
+++ moduloD/exercicios/hora/horas.js
@@ -0,0 +1,28 @@
+function carregar() {
+    var hoje = new Date()
+    var agora = hoje.getHours()
+    var tempo = document.getElementById(`tempo`)
+    var hora = document.getElementById(`hora`)
+    hora.innerHTML = `Agora sao ${agora} horas. `
+
+ if(agora > 23 || agora < 6)  {
+    tempo.innerHTML = `Hora da madrugada`
+    document.body.style.background = `#24022c`
+    img.src = 'madrugada.png'
+}
+else if (agora < 12) {
+    tempo.innerHTML = `Hora do dia`
+    document.body.style.background = `#f5f589`
+    img.src = 'dia.jpeg'
+}
+else if (agora < 18) {
+    tempo.innerHTML = `Hora da tarde`
+    document.body.style.background = `#f3bf7b`
+    img.src = 'tarde.jpeg' 
+}
+else if (agora < 23) {
+    tempo.innerHTML = `Hora da noite`
+    document.body.style.background = `#11013d`
+    img.src = 'noite.jpeg'
+}
+}
