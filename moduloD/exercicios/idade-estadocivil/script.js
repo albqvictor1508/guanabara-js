060d32720fe669afee4d764b622f6dd2acc23316
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-03 23:26:19 -0300

    commit

--- moduloD/exercicios/idade-estadocivil/script.js
+++ moduloD/exercicios/idade-estadocivil/script.js
@@ -10,11 +10,21 @@ function verificar() {
     Id
     var n1 = inputt.value
     var n2 = Boolean(r1.value)
-    var n2 = Boolean(r2.value)
-    var n2 = Boolean(r3.value)
-    var n2 = Boolean(r4.value)
+    var n3 = Boolean(r2.value)
+    var n4 = Boolean(r3.value)
+    var n5 = Boolean(r4.value)
 
     var res = document.getElementById('res')
-    
-    
+    if (n2 == true) {
+        res.innerHTML = `Um/uma ${n2} de ${n1} anos`
+    }
+    else if (n3 == true) {
+
 }
+    else if (n4 == true) {
+
+    }
+    else if (n5 == true) {
+
+    }
+
