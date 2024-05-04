0245e485aad611c299c71c1468c748d527f166b8
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-03 22:11:20 -0300

    Update script.js

--- moduloD/exercicios/idade-estadocivil/script.js
+++ moduloD/exercicios/idade-estadocivil/script.js
@@ -6,13 +6,15 @@ function verificar() {
     var r1 = document.getElementById
     var r2 = document.getElementById
     var r3 = document.getElementById
-    var r4 = document.getElementById
+    var r4 = document.getElementBy
+    Id
     var n1 = inputt.value
-    var n2 = r1.value
-    var n3 = r2.value
-    var n4 = r3.value
-    var n5 = r4.value
+    var n2 = Boolean(r1.value)
+    var n2 = Boolean(r2.value)
+    var n2 = Boolean(r3.value)
+    var n2 = Boolean(r4.value)
 
     var res = document.getElementById('res')
+    
     
 }
