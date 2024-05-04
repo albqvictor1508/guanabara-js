d4fba3256d4c32ae88de2a8b4b4c0947f19a8680
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-03 22:05:40 -0300

    commit

---
+++ moduloD/exercicios/idade-estadocivil/script.js
@@ -0,0 +1,18 @@
+var btn = document.getElementById('btn')
+btn.addEventListener('click', verificar())
+
+function verificar() {
+    var inputt = document.getElementById('inputt')
+    var r1 = document.getElementById
+    var r2 = document.getElementById
+    var r3 = document.getElementById
+    var r4 = document.getElementById
+    var n1 = inputt.value
+    var n2 = r1.value
+    var n3 = r2.value
+    var n4 = r3.value
+    var n5 = r4.value
+
+    var res = document.getElementById('res')
+    
+}
