dfc987198d48c902725946112b09d4673dbbdc11
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-05 00:14:41 -0300

    commit

--- moduloD/exercicios/idade-estadocivil/script.js
+++ moduloD/exercicios/idade-estadocivil/script.js
@@ -1,9 +1,66 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-}
-if (fano.value.length == 0 || Number(fano.value) > ano ) {
-    window.alert('bota alguma coisa ai mermao')
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano ) {
+        window.alert('bota alguma coisa ai mermao')
+    }
+    else {
+        var fsex = document.getElementsByName('radsex')
+        var fcivil = document.getElementsByName('radcivil')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var estado = ''
+        var img = document.createElement('img')
+        if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 16) {
+                img.setAttribute('src', 'imagens/crianca-mulher.jpeg')   
+            }
+            else if (idade <= 18 && fcivil[0].checked) {
+                img.setAttribute('src', 'imagens/NoisNoBanquin.jpeg')
+            }
+            else if (idade <= 21) {
+                img.setAttribute('src', 'imagens/lexsa.jpeg')
+            }
+            else if (idade < 51) {
+                img.setAttribute('src', 'imagens/mulher.png')
+            }
+            else {
+                img.setAttribute('src', 'imagens/veia.jpg')
+            }
+        }
+        else if (fsex[0].checked) {
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imagens/crianca-homem.jpeg')   
+            }
+            else if (idade <= 18 && fcivil[0].checked) {
+                img.setAttribute('src', 'imagens/NoisNoBanquin.jpeg')
+            }
+            else if (idade <= 21) {
+                img.setAttribute('src', 'imagens/perfil.jpg')
+            }
+            else if (idade < 51) {
+                img.setAttribute('src', 'imagens/gigachad.jpeg')
+            }
+            else {
+                img.setAttribute('src', 'imagens/vei.jpg')
+            }
+        }
+        if (fcivil[0].checked) {
+            estado = 'Casado/a'
+        }
+        else if (fcivil[1].checked) {
+            estado = 'Solteiro/a'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero}, ${estado} com ${idade} anos!`
+        res.appendChild(img)
+    }
+    
 }
 
 
