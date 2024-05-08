d7dcaa6605e1c45b4a5301894f23d05143332a2e
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-08 10:44:25 -0300

    commit

---
+++ moduloE/for.js
@@ -0,0 +1,36 @@
+
+//while (teste) {comandos}
+
+//do {comandos} while (teste) 
+
+//for(inicio;teste;incremento) ou for(variavel;teste;comandos)      
+
+
+a = 1  
+b = 1
+
+while (a <= 20) {
+    console.log(`while ${a}`)
+    a++
+}   
+console.log (`agora utilizando o for\n` ) 
+ 
+
+
+
+for (a=1;a<=20;a++) {
+    console.log(`for ${a}`)
+}
+
+
+
+
+do {
+    console.log(b)
+    b++
+
+}
+while (b <= 10 )
+
+
+//todos fazem a mesma coisa, com estruturas diferentes   
