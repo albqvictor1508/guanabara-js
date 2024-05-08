fcede61a3bfecfe9bb07dc5fb26af01aa32229e1
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-08 09:06:52 -0300

    commit

---
+++ moduloE/while-do.js
@@ -0,0 +1,18 @@
+alexsa = 1
+
+do {
+    console.log('alexsa ama victor')
+    alexsa ++
+
+} 
+while (alexsa <= 15)
+
+victor = 1
+
+while (victor <= 15) {
+    console.log('victor ama alexsa')
+    victor ++ 
+}
+
+// while testa primeiro para depois executar, do executa primeiro pra depois testar, fica obvio se olhar para a estrutura dos dois 
+
