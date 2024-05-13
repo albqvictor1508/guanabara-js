f9b11fb14c42f75eb074f2682cd98d23a7b33560
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-13 20:39:47 -0300

    commit

--- moduloF/exercicios/analisador.js
+++ moduloF/exercicios/analisador.js
@@ -30,9 +30,25 @@ function inLista(n, l) {
 }
 
 
-
+item.text = `` 
 function adicionar() {
     if (itsNumber(number.value) <= 0 && itsNumber(number.value) != inLista) {
+        alert('Número inválido')
 
     }
+    else {
+        let selectLista = document.getElementById('lista')
+        
+        arr.push(Number(number.value))
+        let item = document.createElement('option')
+        item.text = `${Number(number.value)} adicionado...`
+        selectLista.appendChild(item)
+        console.log(arr)
+    }
+}
+function finalizar() {
+    let res = document.getElementById('resFinalizar')
+    let resItem = document.createElement('p')
+    resItem.text = `Ao total, teve em média ${arr} números`
+    res.appendChild(resItem)
 }
