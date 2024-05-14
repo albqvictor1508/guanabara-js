f0237d06e83769ed74db1b3697ba35779dc90f35
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-14 10:53:20 -0300

    commit

--- moduloF/exercicios/analisador.js
+++ moduloF/exercicios/analisador.js
@@ -1,6 +1,7 @@
 let btnAdd = document.getElementById('btnAdd')
 let btnFinish = document.getElementById('btnFinish')
 let number = document.getElementById('inputNumber')
+let selectLista = document.getElementById('lista')
 let arr = []
 
 btnAdd.addEventListener('click', adicionar)
@@ -8,7 +9,7 @@ btnFinish.addEventListener('click', finalizar)
 
 function itsNumber(n) {
 
-    if (n > 0 && n <= 100) {
+    if (Number(n) > 0 && Number(n) <= 100) {
         return true
 
         
@@ -21,7 +22,7 @@ function itsNumber(n) {
 }
 function inLista(n, l) {
 
-    if (l.indexOf != -1) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
     }
     else {
@@ -30,19 +31,49 @@ function inLista(n, l) {
 }
 
 
-item.text = `` 
-function adicionar() {
-    if (itsNumber(number.value) <= 0 && itsNumber(number.value) != inLista) {
-        alert('Número inválido')
 
-    }
-    else {
-        let selectLista = document.getElementById('lista')
+function adicionar() {
+    if (itsNumber(number.value) &&  !inLista(number.value, arr)) {
         
         arr.push(Number(number.value))
         let item = document.createElement('option')
+        item.text = `` 
         item.text = `${Number(number.value)} adicionado...`
         selectLista.appendChild(item)
         console.log(arr)
     }
+    else {
+        alert('Número inválido ou já encontrado na lista')
+    }
+    number.value = ``
+    number.focus()
 }
+function finalizar() {
+    let res = document.getElementById('resFinalizar')
+    let maior = arr[0]
+    let menor = arr[0]
+    let soma = 0
+    let media = 0 
+
+    for (let pos in arr) {
+        soma += arr[pos]
+        media += arr[pos] / arr.length
+
+        if(arr[pos] > maior) {
+            maior = arr[pos]
+        }
+        else if (arr[pos] < menor) {
+            menor = arr[pos]
+        } 
+    }
+    res.innerHTML = ''
+    res.innerHTML = `<p>Ao todo, foram postos ${arr.length} valores </p>`
+    res.innerHTML += `<p>O maior número foi ${maior}</p>`
+    res.innerHTML += `<p> O menor número foi ${menor}</p>`
+    res.innerHTML += `<p>A soma entre os números foi ${soma}</p>`
+    res.innerHTML += `<p>A media entre os números foi ${media.toFixed(2)}</p>`
+    
+
+}
+
+//praticar estrutura de repetição, manipulação de array e função composta.
