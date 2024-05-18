b3ae860259c3eae0c53d9547777210d955c4bd04
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-05-18 13:34:49 -0300

    Update repeticao.js

--- moduloF/exercicios3/repeticao.js
+++ moduloF/exercicios3/repeticao.js
@@ -1,6 +1,8 @@
 function nota(n) {
-    let c
-
-    for(c)
+    while (n <= 6) {
+        console.log(`nota baixa seu bosta tirou ${n}`)
+        n++
+    }
+    console.log('passou, tirou', n)
 }
-
+console.log(nota(4))
